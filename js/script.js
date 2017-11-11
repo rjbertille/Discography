9803ce921a4c6b299c00405415ebede00d4f5dac
rjbertille <rjbertille@gmail.com>
2017-11-12 00:43:59 +0100

    Gulp - Sass - Jquery

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("tr").mouseover(function() {
+        $(this).addClass("over");
+    });
+    $("tr").mouseout(function() {
+        $(this).removeClass("over");
+    });
+
+$("tr:even").addClass("even");
+});
